6cccc2efcfc206e0b031fef0b410add200501e79
anikasri <43335325+anikasri@users.noreply.github.com>
2018-09-17 14:38:24 +0530

    Create reversenumber.c

---
+++ reversenumber.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+int a;    
+printf("enter the number");
+scanf("%d,&a");
+if(a=0010)
+{
+printf("enter the value a");
+}
+else
+{
+    printf("error");
+}
+getch();
+}
